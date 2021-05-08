c99602608bbd5ba2d01289e37dc0ffea2883dc78
usamabinnadeem-10 <32700508+usamabinnadeem-10@users.noreply.github.com>
2021-05-08 22:02:31 +0500

    added some comments on Query, please read

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ import base64
 from PIL import Image
 import io
 
-from ..ai import core
+# from ..ai import core
 
 class Query(APIView):
 
@@ -18,8 +18,18 @@ class Query(APIView):
         img_decoded = base64.b64decode(imgstr)
         stream = io.BytesIO(img_decoded)
         img = Image.open(stream).convert("RGBA")
-        img.show()
-        
+
+        """ this would save the image in the directory """ 
+        # img.save('query.png','PNG')
+
+        """ this would show the image but apparently it is not working if the file is large :/  """
+        # img.show()
+
+        """ test with this code a bit and see what works best,
+            saving 'img' first to local storage and then passing the path to the backend_api 
+            OR directly passing the 'img' to the backend_api
+            KEEP IN MIND that you would have to delete the stored image once the model has processed it"""
+
         return Response(
             {
                 'message' : 'This is Query View'
