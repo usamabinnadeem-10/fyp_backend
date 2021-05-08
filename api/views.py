87d9bea106709af47f0732f3a4a6acb40598633c
usamabinnadeem-10 <32700508+usamabinnadeem-10@users.noreply.github.com>
2021-05-08 21:43:42 +0500

    image posted to query endpoint

--- api/views.py
+++ api/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import base64
+from PIL import Image
+import io
 
-# Create your views here.
+from ..ai import core
+
 class Query(APIView):
 
 
     def post(self, request):
+
+        format, imgstr = request.data['query'].split(';base64,')
+
+        img_decoded = base64.b64decode(imgstr)
+        stream = io.BytesIO(img_decoded)
+        img = Image.open(stream).convert("RGBA")
+        img.show()
+        
         return Response(
             {
                 'message' : 'This is Query View'
